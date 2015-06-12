ffe55310c0601a5553403f645ef84373b998133c
Ilkka Harmanen <ilkka.harmanen@bbv.ch>
2015-06-12 13:26:43 +0200

    added building to war for AWS Elastic beanstalk deployment

--- src/main/java/clippy/Application.java
+++ src/main/java/clippy/Application.java
@@ -2,14 +2,23 @@ package clippy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by ilkkaharmanen on 11.06.2015.
  */
+
+
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
