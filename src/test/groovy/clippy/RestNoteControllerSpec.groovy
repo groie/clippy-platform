c82ca9184684083b146cce9f75dde11eb01838bd
Ilkka Harmanen <ilkka.harmanen@bbv.ch>
2015-06-15 14:54:25 +0200

    test with rest assured

--- src/test/groovy/clippy/RestNoteControllerSpec.groovy
+++ src/test/groovy/clippy/RestNoteControllerSpec.groovy
@@ -1,15 +1,12 @@
 package clippy
 
-import groovyx.net.http.RESTClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.IntegrationTest
 import org.springframework.boot.test.SpringApplicationConfiguration
-import org.springframework.boot.test.SpringApplicationContextLoader
-import org.springframework.boot.test.WebIntegrationTest
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.web.WebAppConfiguration
-import spock.lang.Shared
 import spock.lang.Specification
+
+import static com.jayway.restassured.RestAssured.get
 
 /**
  * Created by ilkkaharmanen on 15.06.2015.
@@ -21,20 +18,32 @@ class RestNoteControllerSpec extends Specification {
 
     @Value('${local.server.port}')
     int port
-    def client
-
-    void setup() {
-        client = new RESTClient("http://localhost:${port}")
-    }
+    def path = "/api/v1/"
 
     void "default endpoint answers"() {
         when:
-        def response = client.get([path: "/api/v1/"]);
+        def url = "http://localhost:${port}${path}"
+        def response = get(url)
         then:
         with(response) {
-            status == 200
-            contentType == "text/plain"
+            statusCode == 200
+            contentType ==~ /text\/plain.*/
         }
     }
 
+    void "read non-existing note creates new one and returns it"() {
+        when:
+        def response = get("http://localhost:${port}${path}newnote")
+        then:
+        with(response) {
+            statusCode == 200
+            contentType ==~ /application\/json.*/
+        }
+
+        with(response.jsonPath()) {
+            getString("key") == "newnote"
+            getString("value") == ""
+            getInt("version") == 0
+        }
+    }
 }
