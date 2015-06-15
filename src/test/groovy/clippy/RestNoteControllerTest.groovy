8d3e0f2b3dd059fd4cadc48c0640d620d876cb4c
Ilkka Harmanen <ilkka.harmanen@bbv.ch>
2015-06-15 10:23:47 +0200

    groovy tests set up

---
+++ src/test/groovy/clippy/RestNoteControllerTest.groovy
@@ -0,0 +1,14 @@
+package clippy
+
+import spock.lang.Specification
+
+/**
+ * Created by ilkkaharmanen on 15.06.2015.
+ */
+class RestNoteControllerTest extends Specification {
+
+    void "default endpoint is setup"() {
+        expect:
+        true
+    }
+}
